1f73bb8053a18dcbb13643931f130c5835c13a06
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-30 11:09:39 -0600

    move method call of average age from index to controller

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,7 +1,8 @@
 class LaughTracksApp < Sinatra::Base
 
   get '/comedians' do
-    @comedians = Comedian.all
+      @comedians = Comedian.all.includes(:specials)
+      @average_age = @comedians.avg_age
     erb :index
   end
 end
