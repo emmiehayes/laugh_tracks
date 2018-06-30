3606c9923944c8512b0460f6676bf06ed57c98e8
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-30 10:39:46 -0600

    adjust comedian specials test to be more specific, add model associations and adjust view to match

--- spec/features/user_sees_index_page_spec.rb
+++ spec/features/user_sees_index_page_spec.rb
@@ -11,5 +11,16 @@ RSpec.describe 'a visitor' do
       expect(page).to have_content("Name: #{comedian_1.name}")
       expect(page).to have_content("Age: #{comedian_2.age}")
     end
+
+    it "should see a list of all comedian specials" do
+      comedian_1 = Comedian.create(name: 'Jimmy Fallon', age: 34)
+
+      special_1 = comedian_1.specials.create(name: 'Special Number One')
+      special_2 = comedian_1.specials.create(name: 'Special Number Two')
+
+      visit "/comedians"
+      expect(page).to have_content("#{comedian_1.name} Special: #{special_1.name}")
+      expect(page).to have_content("#{comedian_1.name} Special: #{special_2.name}")
+    end
   end
 end
