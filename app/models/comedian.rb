192ce55bfb094260345a1f9fb45649ad103e5908
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-30 10:23:55 -0600

    update first test to be more specific, adjust view to allow for specifics

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,6 +1,8 @@
 class Comedian < ActiveRecord::Base
   validates_presence_of :name, :age
 
+  has_many :specials
+
   def self.average_age
     average(:age)
   end
--- spec/features/user_sees_index_page_spec.rb
+++ spec/features/user_sees_index_page_spec.rb
@@ -1,4 +1,4 @@
-RSpec 'a visitor'
+RSpec.describe 'a visitor' do
   context "visiting /comedians" do
     it "should see a list of all comedians" do
       comedian_1 = Comedian.create(name: 'Jimmy Fallon', age: 34)
@@ -6,29 +6,10 @@ RSpec 'a visitor'
 
       visit "/comedians"
 
-      expect(page).to have_content(comedian_1.name)
-      expect(page).to have_content(comedian_1.age)
-      expect(page).to have_content(comedian_2.name)
-      expect(page).to have_content(comedian_2.age)
-    end
-
-    it "should see an average age for all comedians" do
-      comedian_1 = Comedian.create(name: 'Jimmy Fallon', age: 34)
-      comedian_2 = Comedian.create(name: 'Judd Apatow', age: 45)
-
-      visit "/comedians"
-
-      expect(page).to have_content(comedian_1.special)
-    end
-
-    it "should show a comedian's special" do
-      comedian = Comedian.create(name: 'Jimmy Fallon', age: 34)
-      comedian.special.create
-      special = comedian.special.create(name:'Fallon special')
-
-      visit "/comedians"
-
-      expect(page).to have_content(comedian.special)
+      expect(page).to have_content("Name: #{comedian_1.name}")
+      expect(page).to have_content("Age: #{comedian_1.age}")
+      expect(page).to have_content("Name: #{comedian_1.name}")
+      expect(page).to have_content("Age: #{comedian_2.age}")
     end
   end
 end
